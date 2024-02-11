3db26c2ac0101592bab074fdf06d8264d318d65f
Distortion <agamanstha41@gmail.com>
2024-02-11 19:32:33 +0545

    Updated home page
    -Completely changed the look of the page with the addition of hero page.
    -Added curve SVGs.
    -Added animation for SVGs ( not smooth ).
    -Addition of basic data in contact page.
    -Deletion of iaa.html

--- artweb.js
+++ artweb.js
@@ -2,25 +2,46 @@ var popContent = document.getElementById("popup1");
 var overlayWithPopUp = document.getElementById("overlay");
 var closeButton = document.getElementById("closepopup")
 
+
 $("button#shopitem").click(buttonOnClick);
 
 function buttonOnClick() { 
     popContent.style.display = "flex"
     overlayWithPopUp.style.display = "block"
     closeButton.style.display = "block"
-    // console.log("tt")
+ 
 };
 
 $("#closepopup").click(function() {
-    // console.log("Test")
+   
     popContent.style.display = "none"
     overlayWithPopUp.style.display = "none"
     closeButton.style.display = "none"
 });
 
-// MAN so getElementById works but it only works on one button ---#SOLVED---
-// I'm trying to use getElementByClass but looks like the syntax is different. I am done for today.
 
 
-// 24x60in thumb1
-// 24x48in thumb2
+// Load More Button \\
+var revealImages = document.getElementById("gallery_Alt");
+var clickcounter = 0;
+
+$("button#more").click(revealGallery);
+
+function revealGallery(){
+    clickcounter++
+    if(clickcounter == 1){ 
+    revealImages.style.display = "grid"
+    }
+    else{
+    revealImages.style.display = "none"
+    clickcounter = 0; 
+}
+    
+} 
+
+
+// Animation \\
+    
+
+   
+
