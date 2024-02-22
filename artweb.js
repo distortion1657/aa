07b8ca881c52c2e6ed085542ff31591b7d410c3a
Distortion <agamanstha41@gmail.com>
2024-02-22 15:19:31 +0545

    Major Page Changes
    - Removed the SVGs previously added.
    - Migration of Gallery from the Home page to it's own page.
    - Added About me section
    - Added transition animations for elements of the page.

--- artweb.js
+++ artweb.js
@@ -3,14 +3,55 @@ var overlayWithPopUp = document.getElementById("overlay");
 var closeButton = document.getElementById("closepopup")
 
 
-$("button#shopitem").click(buttonOnClick);
 
+const hiddenElements = document.querySelectorAll(".hidden")
+const slidingElements = document.querySelectorAll(".slide-in")
+const slideDownElements = document.querySelectorAll(".slide-down")
+const config = {
+    
+    threshold: 0.3
+}
+
+const slowConfig = {
+    threshold: 1
+}
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        
+        if(entry.isIntersecting && entry.target.classList.contains("slide-in")){
+           
+            entry.target.classList.add("slide-finish")
+        
+        }else if (entry.isIntersecting && entry.target.classList.contains("hidden")){
+            entry.target.classList.add("show")
+
+        }else if (entry.isIntersecting && entry.target.classList.contains("slide-down")){
+            entry.target.classList.add("slide-finish")
+            observer.unobserve(entry.target)
+        };
+        });
+    });
+
+
+slideDownElements.forEach((el) => observer.observe((el)));
+hiddenElements.forEach((el) => observer.observe((el)));
+slidingElements.forEach((el) => observer.observe((el)))
+
+
+
+
+
+
+
+$("button#shopitem").click(buttonOnClick);
 function buttonOnClick() { 
     popContent.style.display = "flex"
     overlayWithPopUp.style.display = "block"
     closeButton.style.display = "block"
  
 };
+
+
 
 $("#closepopup").click(function() {
    
@@ -19,20 +60,28 @@ $("#closepopup").click(function() {
     closeButton.style.display = "none"
 });
 
-
-
 // Load More Button \\
-var revealImages = document.getElementById("gallery_Alt");
+var revealImages = document.getElementById("gallery-load-more");
 var clickcounter = 0;
 
 $("button#more").click(revealGallery);
 
 function revealGallery(){
     clickcounter++
-    if(clickcounter == 1){ 
+    if(clickcounter === 1){
+    
     revealImages.style.display = "grid"
+    window.scrollBy({
+        top: 400,
+        behavior: "smooth"
+    }
+    );
     }
     else{
+        window.scrollBy({
+            bottom: 400,
+            behavior: "smooth"
+        })
     revealImages.style.display = "none"
     clickcounter = 0; 
 }
---
+++ text.md
@@ -0,0 +1,4 @@
+Hello, I am Ananda Kumar Shrestha, a self-taught artist who finds peace in painting. I specialize in Acrylic colors but my skills do not lack 
+on the other aspects of art. I've worked with countless clients to tailor them a piece of their liking and delivering something they would be 
+proud to hang on their walls. If you have an empty wall, why not decorate it with an elegant artwork that will certainly be a conversation starter.
+Your satisfaction is my gratification.
